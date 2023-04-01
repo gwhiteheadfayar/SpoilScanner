58f39533202d9c8f34481e2ac504b5d5678f9e03
JBobda <J.BobdaT@gmail.com>
2023-03-31 20:33:09 -0500

    Added firebase to the server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,28 @@
-const express = require('express');
+import express from 'express';
+import { config } from './resources/config.js';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: config.apiKey,
+    authDomain: "spoilscanner.firebaseapp.com",
+    databaseURL: "https://spoilscanner-default-rtdb.firebaseio.com",
+    projectId: "spoilscanner",
+    storageBucket: "spoilscanner.appspot.com",
+    messagingSenderId: config.messagingSenderId,
+    appId: config.appId,
+    measurementId: config.measurementId
+};
+
+// Initialize Firebase
+const fireApp = initializeApp(firebaseConfig);
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 })
